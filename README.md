ec0e8dbc29cd6ac82112f6b37061d7243a38a849
François Roland <francois.roland@heh.be>
2025-09-21 17:58:08 +0200

    Write TD2 tests.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# base
+# TD2 - calculatrice
 
-Consignes
+Crée un programme en TypeScript qui contient **quatre fonctions** distinctes :
+
+1. `addition(nombre1: number, nombre2: number): number`
+2. `soustraction(nombre1: number, nombre2: number): number`
+3. `multiplication(nombre1: number, nombre2: number): number`
+4. `division(nombre1: number, nombre2: number): number`
+
+Écris le code qui retourne la réponse correspondant au nom de la fonction.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,21 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {addition, soustraction, multiplication, division} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('Calculatrice', () => {
+    test('Additionne deux nombres', () => {
+        expect(addition(12, 6)).toEqual(18);
+    });
+
+    test('Soustrait deux nombres', () => {
+        expect(soustraction(12, 6)).toEqual(6);
+    });
+
+    test('Multiplie deux nombres', () => {
+        expect(multiplication(12, 6)).toEqual(72);
+    });
+
+    test('Divise deux nombres', () => {
+        expect(division(12, 6)).toEqual(2);
     });
 });
